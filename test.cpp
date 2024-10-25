c7fb6e584af24e8f27c6c7774171b3b17bfba1f2
Adrian R. Anaya <142830862+ADI-Trickster@users.noreply.github.com>
2024-10-25 13:00:00 -0500

    test 1 for summzation
    currently always goes to infit
    
    mathmanterry wanted to see how it would like for in as a script

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+double tele(int num){
+    double daSum=0.0;
+
+    for(int i=0; i<= num; i++){
+        daSum += 1.0/i - 1.0/(i+1);
+    }
+
+    return daSum;
+}
+
+int main() {
+    int limitUpperBound;
+
+    cout<< "pick an upper limit: ";
+    cin >> limitUpperBound;
+
+    double res =  tele(limitUpperBound);
+
+    cout << res;
+
+    return 0;
+}
